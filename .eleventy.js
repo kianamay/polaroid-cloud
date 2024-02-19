ceb924e2645703ed670602b70c08eae1e2d01da0
kianamay <maykiana@msu.edu>
2024-02-19 02:38:39 -0500

    base before

--- .eleventy.js
+++ .eleventy.js
@@ -2,4 +2,6 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("styles");
+  eleventyConfig.addPassthroughCopy("_includes");
+  eleventyConfig.addPassthroughCopy("index.liquid");
 };
